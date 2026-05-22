af1efc70e1f0b02ff54ca4a5104b900b37d7e910
nishida namiki <ymdnmk1201@gmail.com>
2026-05-22 17:29:17 +0900

    feat: カウントダウンタイマーを追加

--- async-practice.js
+++ async-practice.js
@@ -12,3 +12,23 @@ setTimeout(() => {
 }, 1000);
 
 console.log("E");
+
+// 課題2：カウントダウンタイマー
+function countdown(seconds) {
+  console.log(`カウントダウン開始: ${seconds}秒`);
+
+  for (let i = seconds; i >= 0; i--) {
+    setTimeout(
+      () => {
+        if (i > 0) {
+          console.log(i);
+        } else {
+          console.log("完了！");
+        }
+      },
+      (seconds - i) * 1000,
+    );
+  }
+}
+
+countdown(10);
