c5a2a7bd54747c82e9718d0d77ee47b5332afde6
nishida namiki <ymdnmk1201@gmail.com>
2026-05-24 14:56:07 +0900

    feat: fetchで取得したユーザー一覧を表示

--- fetch-basic.js
+++ fetch-basic.js
@@ -14,3 +14,25 @@ async function fetchData() {
 }
 
 fetchData();
+
+// JSONデータを取得して画面に表示
+async function displayUsers() {
+  // ユーザーリストを表示するul要素を取得
+  const userList = document.querySelector("#user-list");
+
+  // 1.データを取得する
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  // 2.取得したユーザーデータの配列をループ処理
+  users.forEach((user) => {
+    // 3.各ユーザーごとにli要素を生成
+    const listItem = document.createElement("li");
+    listItem.textContent = user.name;
+
+    // 4.ul要素にli要素を追加
+    userList.appendChild(listItem);
+  });
+}
+
+displayUsers();
