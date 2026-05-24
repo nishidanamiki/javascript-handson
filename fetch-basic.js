f7a7fb711facc156c23a14b61d33c2ab3cd50816
nishida namiki <ymdnmk1201@gmail.com>
2026-05-24 14:31:44 +0900

    feat: fetch APIでJSONデータを取得

--- fetch-basic.js
+++ fetch-basic.js
@@ -1,8 +1,16 @@
-// fetchの基本構文
+// fetch APIでレスポンスとJSONデータを取得
 async function fetchData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
 
   console.log(response);
+
+  if (!response.ok) {
+    console.error("データの取得に失敗しました");
+    return;
+  }
+
+  const post = await response.json();
+  console.log(post);
 }
 
 fetchData();
